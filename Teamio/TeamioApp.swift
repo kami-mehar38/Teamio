8c951d487dc0f40242e5a613fbcd7a52096ceeb9
Kamran <kamran@skyloov.com>
2021-05-05 20:31:11 +0400

    Initial Commit

---
+++ Teamio/TeamioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamioApp.swift
+//  Teamio
+//
+//  Created by Kamran Ramzan on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct TeamioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
